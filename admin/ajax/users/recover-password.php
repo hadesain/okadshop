f7cc855420a8a69649f353f3854482ac069cb4d3
dev2moullablad <dev2@moullablad.com>
2016-06-30 14:16:01 +0000

    Admin login form design
    + Change the look of backoffice login form
    + Add reset password functionality

---
+++ admin/ajax/users/recover-password.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * 2016 OkadShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@okadshop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade OkadShop to newer
+ * versions in the future. If you wish to customize OkadShop for your
+ * needs please refer to http://www.okadshop.com for more information.
+ *
+ * @author    OkadShop <contact@okadshop.com>
+ * @copyright 2016 OkadShop
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of OkadShop
+ */
+
+
+//This is an ajax request
+if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) 
+&& strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest')
+{
+	die();
+}
+
+include '../../../config/bootstrap.php';
+
+
+//prepare data
+global $common;
+$receiver = $_POST['email'];
+if( $receiver == "" ) return;
+
+//check if user exist
+$exist = $common->select("users", array('id'), "WHERE email='".$receiver."'");
+if( !$exist[0]['id'] ){
+	$return['error']  = l("No user registered with that email address.", "admin");
+	echo json_encode( $return );
+	exit;
+}
+
+$headers  = "From: ".APP." <no-reply@okadshop.com>\r\n". 
+						"MIME-Version: 1.0" . "\r\n" . 
+						"Content-type: text/html; charset=UTF-8" . "\r\n"; 
+$subject  = 'Reset Your Password';
+$message  = '<strong>Hi,</strong><br>';
+$message .= 'We got a request to reset your admin aria pasword.<br>';
+$message .= 'This is your new password : <br>';
+$success  = mail($receiver, $subject, $message, $headers); 
+
+//return message
+if( $success ){
+	$return['success'] = l("Email with password was sent to your Inbox.", "admin");
+	echo json_encode( $return );
+}else{
+	$return['error'] = l("Error sending email email.", "admin");
+	echo json_encode( $return );
+}
--- admin/includes/plugins/users/login/login.php
+++ admin/includes/plugins/users/login/login.php
@@ -1,148 +1,171 @@
 <?php
-require_once 'header.php';
-$Security = new Security();
-$DB = Database::getInstance();
+require "../config/bootstrap.php"
 ?>
-<style>
-@CHARSET "UTF-8";
+<!DOCTYPE html>
+<html dir="<?=$os_direction;?>" lang="<?=$os_lang;?>">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta charset="<?=$os_encoding;?>">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="favicon.ico">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title><?=l("Admin Aria", "admin");?></title>
+  <!-- Boosttrap -->
+  <link href="assets/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
+  <link href="assets/css/font-awesome.min.css" type="text/css" rel="stylesheet" />
+  <!-- http://bootsnipp.com/snippets/featured/google-style-login-extended-with-html5-localstorage -->
+  <link href="assets/css/login_form.css" rel="stylesheet" type="text/css" />
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body>
 
-* {
-    -webkit-box-sizing: border-box;
-       -moz-box-sizing: border-box;
-            box-sizing: border-box;
-    outline: none;
-}
-
-html {
-  height: 100%;
-}
-
-body {
-    background: #A5245E !important;
-    min-height: 100%;
-}
+  <div class="container">
+    <div class="card card-container">
+      <img id="profile-img" class="profile-img-card" src="assets/images/avatar.png" />
+      <center>
+        <strong>OkadShop <?=_OS_VERSION_;?></strong>
+      </center>
+      <p id="profile-name" class="profile-name-card"></p>
+      <div class="alert alert-danger" id="alert" style="display:none;">
+        <a type="button" class="close" data-dismiss="alert">x</a>
+        <strong>fdsfd</strong>
+      </div>
 
 
-.login-form {
-    max-width: 400px;
-    margin: 0 auto;
-}
-
-form[role=login] {
-    font: 16px/1.6em Lato, serif;
-    color: #A5245E;
-    background: #fff;
-    margin-top: 100px;
-    padding: 0 50px 25px 50px;
-    border-radius: 5px;
-    -moz-border-radius: 5px;
-    -webkit-border-radius: 5px;
-}
-form[role=login] > section {
-    text-align: center;
-    font-size: 14px;
-    margin-top: 2em;
-}
-    form[role=login] section a {
-        color: #A5245E;
-    }
-form[role=login] h3 {
-    font-size: 30px;
-    text-align: center;
-    color: #c0c0c0;
-    padding: 35px 0 12px 0;
-}
-    form[role=login] h3 b {
-        color: #A5245E;
-    }
-
-    form[role=login] span.glyphicon {
-        color: #adadad;
-    }
-    form[role=login] input,
-    form[role=login] button {
-        font-size: 16px;
-        margin: 5px 0;
-    }
-    form[role=login] input {
-        color: #c1c4c5;
-        background: #fafafa;
-        border: none;
-        padding-left: 40px;
-        border: 1px solid #EDEDED;
-    }
-        form[role=login] input::-webkit-input-placeholder {
-            color: #c1c4c5;
-        }
-        form[role=login] input:-moz-placeholder {
-            color: #c1c4c5;
-        }
-        form[role=login] input::-moz-placeholder {
-            color: #c1c4c5;
-        }
-        form[role=login] input:-ms-input-placeholder {  
-            color: #c1c4c5;
-        }
-    form[role=login] button {
-        line-height: 30px;
-        font-weight: bold;
-        -webkit-box-shadow: 0px 3px 0px 0px rgba(24, 121, 158, 1);
-           -moz-box-shadow: 0px 3px 0px 0px rgba(24, 121, 158, 1);
-                box-shadow: 0px 3px 0px 0px rgba(24, 121, 158, 1);
-        background: #3796ba;
-        border: none;
-    }
-    form[role=login] button:hover {
-        background: #4eaccf;
-    }
-    
-    .btn.btn-success {
-        background-color: #3E9E28;
-    }
-
-.login-form .form-control {
-    padding-left: 40px;
-}
-.login-form .form-control + .glyphicon {
-    position: absolute;
-    left: 0;
-    top: 22%;
-    padding: 6px 0 0 27px;
-}
-
-    h5{
-        color: white;
-        font-size: 40px;
-        margin-top: 39px;
-        margin-left: 68px;
-        position: absolute;
-        opacity: 0.2;
-        font-weight: bold;
-    }
-</style>
-
-<section class="container">
-    <section class="login-form">
-    <form method="post" action="" role="login">
-      <input type="hidden" name="class" value="login">
-        <h3><b><?=l("OkadShop", "admin");?></b> <?=l("Connexion", "admin");?></h3>
-        <div class="row">
-            <div class="col-xs-12">
-                <input type="email" name="email" placeholder="Adresse &eacute;lectronique" required class="form-control input-lg" />
-                <span class="glyphicon glyphicon-user"></span>
-            </div>
-            <div class="col-xs-12">
-                <input type="password" name="password" placeholder="Mot de passe" required class="form-control input-lg" />
-                <span class="glyphicon glyphicon-lock"></span>
-            </div>
+      <form method="post" action="" role="login" class="form-signin">
+        <input type="hidden" name="class" value="login">
+        <span id="reauth-email" class="reauth-email"></span>
+        <input type="email" id="inputEmail" name="email" placeholder="<?=l("Email address", "admin");?>" class="form-control"  required autofocus />
+        <input type="password" id="inputPassword" name="password" placeholder="<?=l("Password", "admin");?>" class="form-control" required />
+        <div id="remember" class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> <?=l("Remember me", "admin");?>
+          </label>
         </div>
-        <button type="submit" id="con" value="connexion" class="btn btn-lg btn-block btn-success"><?=l("SE CONNECTER", "admin");?></button>
-        <section>
-            <a class="retoure" href="../"><?=l("Retour au Site", "admin");?></a>
-        </section>
-    </form>
-    </section>
-</section>
-<?php
-die();
-?>
+        <button type="submit" id="con" value="connexion" class="btn btn-lg btn-primary btn-block btn-signin"><?=l("Sign in", "admin");?></button>
+        <a class="retoure" href="../"><i class="fa fa-long-arrow-left"></i><?=l("My shop", "admin");?></a>
+        <a id="forgot-password" class="pull-right"><?=l("Forgot the password?", "admin");?></a>
+      </form><!-- /form -->
+
+      <form method="post" action="" class="form-password hidden">
+        <div class="alert alert-info">
+          <h4>Forgot your password?</h4>
+          <p>In order to receive your access code by email, please enter the address you provided during the registration process.</p>
+        </div>
+        <input type="email" id="email" placeholder="<?=l("Email address", "admin");?>" class="form-control" required />
+        <a id="back-login"><i class="fa fa-long-arrow-left"></i><?=l("Back to login", "admin");?></a>
+        <a id="send-password" class="btn btn-default btn-xs pull-right"><?=l("Send", "admin");?></a>
+      </form><!-- /form -->
+
+    </div><!-- /card-container -->
+
+    <div id="login-footer">
+      <p class="text-center text-muted">
+        <a href="http://www.okadshop.com/" target="_blank">
+          <?=l("© OkadShop 2016 - All rights reserved", "admin");?>
+        </a>
+      </p>
+      <p class="text-center">
+        <a href="https://twitter.com/OKADshop" target="_blank" title="Twitter">
+          <i class="fa fa-twitter tw_color"></i>
+        </a>
+        <a href="https://www.facebook.com/OKADshop/" target="_blank" title="Facebook">
+          <i class="fa fa-facebook fb_color"></i>
+        </a>
+        <a href="https://github.com/softhightech/okadshop" target="_blank" title="Github">
+          <i class="fa fa-github git_color"></i>
+        </a>
+        <a href="https://plus.google.com/112035582044146693504" target="_blank" title="Google">
+          <i class="fa fa-google-plus gp_color"></i>
+        </a>
+        <a href="https://www.youtube.com/channel/UC_NLvUcKRmqUq_arEKn7KlA" target="_blank" title="Youtube">
+          <i class="fa fa-youtube yt_color"></i>
+        </a>
+      </p>
+    </div>
+
+
+  </div><!-- /container -->
+
+
+  <!-- jQuery Library -->
+  <script src="assets/js/jquery.min.js" type="text/javascript"></script>
+  <script>
+  $( document ).ready(function() {
+
+    //forgot password form
+    $("#forgot-password").on('click', function(){
+      $(".form-signin").addClass('hidden');
+      $(".form-password").removeClass('hidden');
+    });
+
+    //back to login form
+    $("#back-login").on('click', function(){
+      $(".form-signin").removeClass('hidden');
+      $(".form-password").addClass('hidden');
+    });
+
+    //send password by email
+    $("#send-password").on('click', function(){
+      recover_user_password( $("#email").val() );
+    });
+
+
+
+  });
+
+  function recover_user_password(email){
+    var valid_email = ckeck_email_address(email);
+    if( email == "" ){
+      show_alert_message("<?=l('Address email is required.');?>", "danger");
+      return false;
+    }else if(!valid_email){
+      show_alert_message("<?=l('Please enter a valid email address.');?>", "danger");
+      return false;
+    }
+    $.ajax({
+      type: "POST",
+      data: {email:email},
+      url: 'ajax/users/recover-password.php',
+      success: function(response){
+        try {
+          var data = JSON.parse(response);
+          if( data.error ){
+            show_alert_message(data.error, "danger");
+          }else if( data.success ){
+            show_alert_message(data.success, "success");
+          }else{
+            show_alert_message("<?=l('An error has occurred !');?>", "danger");
+          }
+        }catch (e) {
+          show_alert_message("<?=l('An error has occurred !');?>", "danger");
+        }
+      }
+    });
+  }
+
+  function show_alert_message(message, type){
+    if( type == "danger" ){
+      $("#alert").removeClass("alert-success").addClass("alert-danger");
+    }else{
+      $("#alert").removeClass("alert-danger").addClass("alert-success");
+    }
+    $("#alert").show();
+    $("#alert strong").text(message);
+    $("#alert").fadeTo(2000, 500).slideUp(500);
+    return false;
+  }
+
+  function ckeck_email_address(email) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(email);
+  }
+  </script>
+</body>
+</html>
